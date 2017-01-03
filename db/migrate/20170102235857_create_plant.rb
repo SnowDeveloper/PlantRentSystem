4cae117c397c4f432c94581efd06090727f425e1
Madhushree Singh <contact.madhushreesingh@gmail.com>
2017-01-03 02:00:27 +0200

    Initial Commit

---
+++ db/migrate/20170102235857_create_plant.rb
@@ -0,0 +1,6 @@
+class CreatePlant < ActiveRecord::Migration[5.0]
+  def change
+    create_table :plants do |t|
+    end
+  end
+end
---
+++ db/migrate/20170102235917_create_purchase_order.rb
@@ -0,0 +1,6 @@
+class CreatePurchaseOrder < ActiveRecord::Migration[5.0]
+  def change
+    create_table :purchase_orders do |t|
+    end
+  end
+end
